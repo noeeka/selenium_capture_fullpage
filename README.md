d1fc8b2dbf9d2d98772fc1528e60ad776b2eb9f4
Eric <noeeka@126.com>
2022-01-18 20:15:48 +0800

    add code

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# selenium_capture_fullpage
+---
+title: Selenium with python to capture full page 
+tags: selenium,python,automation,testing
+category: noeeka
+renderNumberedHeading: true
+grammar_cjkRuby: true
+---
+
+
+## Introduction
+This script used selenium and webdriver to capture full page in Desktop & Mobile view. There are 29 websites home page in total. For each site to generate full page capture in Desktop & Mobile
+
+## Runtime Enviroment
+
+|  Module  | Name    |   Version  |     |
+| --- | --- | --- | --- |
+| OS   | Windows   |   10+  |     |
+|   Brower | Chrome    |   97.0.4692.71   |     |
+|   Webdriver| chromedriver   |   98.0.4758.48  |     |
+|   Python| X64  |   3.9  |     |
+|   Selenium|  |   4.1.0  |     |
+
+## pre-requirement
+
+|    PIP Module |  version   |
+| --- | --- |
+|   docx  |  0.8.11   |
+|   selenium |  4.1.0   |
+
+``` apache
+pip install -v docx == 0.8.11
+pip install -v selenium == 4.1.0
+```
+
+## How to run
+Put chromedriver into the path as same as Chrome in OS
+at script directory, run `python fullpage.py`
---
+++ fullpage.py
@@ -0,0 +1,73 @@
+import time
+from docx import Document
+from docx.shared import Inches
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+lists = ['bh', 'bd', 'bn', 'in', 'id', 'my', 'pk', 'sg', 'lk', 'vn', 'hk', 'cn', 'tw', 'ci', 'fk', 'gm', 'gh', 'je',
+         'je', 'jo', 'ke', 'np', 'ng', 'sl', 'tz', 'ae', 'ug', 'zm', 'bw', 'zw']
+
+document = Document()
+for x in lists:
+    url = "https://www.sc.com/" + x
+
+    image_desktop_full = "image_desktop_full.png"
+    image_mobile_full = "image_mobile_full.png"
+    option = webdriver.ChromeOptions()
+
+    # capture for desktop option
+    option.add_argument('--headless')
+    option.add_argument('--disable-gpu')
+    option.add_argument("--window-size=1920,1080")
+    option.add_argument("--hide-scrollbars")
+
+    driver = webdriver.Chrome(chrome_options=option)
+
+    driver.get(url)
+    print(driver.title)
+    print(url)
+    time.sleep(8)
+    scroll_width = driver.execute_script('return document.body.parentNode.scrollWidth')
+    scroll_height = driver.execute_script('return document.body.parentNode.scrollHeight')
+    driver.set_window_size(scroll_width, scroll_height)
+    driver.save_screenshot(image_desktop_full)
+
+    # capture for mobile option
+    option.add_argument('--headless')
+    option.add_argument('--disable-gpu')
+    option.add_argument("--window-size=390,884")
+    option.add_argument("--hide-scrollbars")
+
+    driver = webdriver.Chrome(chrome_options=option)
+
+    driver.get(url)
+    print(driver.title)
+    print(url)
+    time.sleep(8)
+    scroll_width = driver.execute_script('return document.body.parentNode.scrollWidth')
+    scroll_height = driver.execute_script('return document.body.parentNode.scrollHeight')
+    driver.set_window_size(scroll_width, scroll_height)
+    driver.save_screenshot(image_mobile_full)
+    driver.quit()
+
+
+    # 初始化建立第一个自然段
+    pl = document.add_paragraph()
+    # 对齐方式为居中，没有这句话默认左对齐
+    run1 = pl.add_run(url)
+
+    document.sections[0].left_margin = Inches(0.3)
+    document.sections[0].bottom_margin = Inches(0.3)
+
+
+
+
+
+    document.add_picture(image_desktop_full, height=Inches(9))
+
+    new_section = document.add_section()
+    document.add_picture(image_mobile_full, height=Inches(9))
+
+# document.add_page_break()
+document.save('report.docx')
+print("In total:" + str(len(lists)))
